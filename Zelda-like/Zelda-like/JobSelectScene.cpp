5d9278f569ced5c9af07c308bee2f18c50824ebc
Hyunwoo Moon <moonhyunwoo.dev@gmail.com>
2026-08-03 15:18:06 +0900

    Add Zelda-like project and SFML setup
    Add a new 'Zelda-like' module: many stubbed source and header files (Animation, Camera, DataManager, Entity, EntityManager, Game, InputManager, JobSelectScene, Monster, Player, Scene, SceneManager, TitleScene) plus Scene interface. Update Zelda-like.vcxproj/.filters to use C++17, include SFML include/lib paths and SFML dependencies, and add new files to the project and filter hierarchy. Replace main.cpp to create an SFML window and render a green circle. Also update SFML_Practice project files to include networking/source files (main.cpp, TcpClient.cpp, TcpServer.cpp, Udp.cpp) and corresponding filters.

---
+++ Zelda-like/Zelda-like/JobSelectScene.cpp
@@ -0,0 +1,1 @@
+#include "JobSelectScene.h"
---
+++ Zelda-like/Zelda-like/Scene.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Scene
+{
+public:
+	virtual ~Scene() = default;
+
+	virtual void HandleEvent(const sf::Event& event) = 0;
+	virtual void Update(float deltaTime) = 0;
+	virtual void Render(sf::RenderWindow& window) = 0;
+};
+
